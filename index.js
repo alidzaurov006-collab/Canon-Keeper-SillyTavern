f58bdc8262a41cea1c4432c71bdf42a46114c8e6
alidzaurov006-collab <alidzaurov006@gmail.com>
2026-08-21 04:16:52 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    console.log('[Canon Keeper] запущен');
+
+    const test = document.createElement('div');
+    test.id = 'canon-keeper-test';
+    test.textContent = 'Canon Keeper ✓';
+
+    document.body.appendChild(test);
+})();
