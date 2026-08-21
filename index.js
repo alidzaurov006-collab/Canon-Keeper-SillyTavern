46b41836d12fe90ad0223b2ca96ffac45a4bed18
alidzaurov006-collab <alidzaurov006@gmail.com>
2026-08-21 05:25:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ jQuery(function () {
     const buttonHtml = `
         <div id="canon-keeper-button"
              class="list-group-item flex-container flexGap5">
-            <div class="fa-solid fa-shield-halved extensionsMenuExtensionButton"></div>
+            <div class="fa-solid fa-book extensionsMenuExtensionButton"></div>
             Canon Keeper
         </div>
     `;
@@ -11,30 +11,11 @@ jQuery(function () {
     $('#extensionsMenu').prepend(buttonHtml);
 
     $('#canon-keeper-button').on('click', function () {
-        openCanonKeeper();
+        alert('Canon Keeper работает!');
     });
 
-    function openCanonKeeper() {
-
-        if ($('#canon-keeper-panel').length) {
-            $('#canon-keeper-panel').remove();
-            return;
-        }
-
-        const panelHtml = `
-            <div id="canon-keeper-panel">
-
-                <div class="ck-window">
-
-                    <div class="ck-header">
-
-                        <div class="ck-title">
-                            🛡️ CANON KEEPER
-                            <small>Хранитель канона</small>
-                        </div>
-
-                        <button id="ck-close" type="button">
-                            ×
+    console.log('[Canon Keeper] button added');
+});                            ×
                         </button>
 
                     </div>
